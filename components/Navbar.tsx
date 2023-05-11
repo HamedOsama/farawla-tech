9676cf0bfc5b077c7cdb4882cfb01a6f76dfc236
Hamed Osama <hamedosama19@gmail.com>
2023-05-11 15:54:51 +0200

    add locale links to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,22 +9,22 @@ export default async function Navbar({ lng }: { lng: string }) {
     <nav className="w-full py-12">
       <ul className='w-full flex items-center justify-center gap-12 sm:gap-24 md:gap-36 lg:gap-48 text-lg sm:text-2xl'>
         <li>
-          <Link href="/" className='text-white'>
+          <Link href={`/${lng}`} className='text-white'>
             {t('navbar.home')}
           </Link>
         </li>
         <li>
-          <Link href="/about" className='text-white'>
+          <Link href={`/${lng}/about`} className='text-white'>
             {t('navbar.about')}
           </Link>
         </li>
         <li>
-          <Link href="/news" className='text-white'>
+          <Link href={`/${lng}/news`} className='text-white'>
             {t('navbar.news')}
           </Link>
         </li>
         <li>
-          <Link href="/contact" className='text-white'>
+          <Link href={`/${lng}/contact`} className='text-white'>
             {t('navbar.contact')}
           </Link>
         </li>
