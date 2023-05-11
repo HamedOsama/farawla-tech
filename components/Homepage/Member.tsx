616018b9b9eb9227338fc12111cd1ba5ba195080
Hamed Osama <hamedosama19@gmail.com>
2023-05-11 23:54:24 +0200

    transform from framer-motion to aos

--- components/Homepage/Member.tsx
+++ components/Homepage/Member.tsx
@@ -1,7 +1,6 @@
 "use client"
-import { useInView } from 'framer-motion'
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React from 'react'
 
 interface MemberProps {
   name: string
@@ -12,16 +11,10 @@ interface MemberProps {
 }
 
 const Member = ({ name, position, faculty, university, img }: MemberProps) => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: false, margin: '10%' });
   return (
     <div
       className='flex items-center justify-center flex-col duration-700 ease-in-out'
-      ref={ref}
-      style={{
-        opacity: isInView ? 1 : 0,
-        transform: isInView ? 'translateY(0)' : 'translateY(250px)',
-      }}
+      data-aos="fade-up"
     >
       <div className="relative rounded-[50%] w-36 h-36 overflow-hidden mb-6 cursor-pointer">
         {/* <div className="absolute inset-0 bg-black/50 duration-300 ease-in-out hover:bg-transparent"></div> */}
