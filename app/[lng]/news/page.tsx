6af0979b1b7d7419205ef40ef098c24f4a7c6f1d
Hamed Osama <hamedosama19@gmail.com>
2023-05-14 19:32:45 +0300

    update news page

--- app/[lng]/news/page.tsx
+++ app/[lng]/news/page.tsx
@@ -14,46 +14,47 @@ const page = async ({ params: { lng } }: IParams) => {
   const { t } = await useTranslation(lng)
 
   return <main className='flex flex-col'>
-    
+
     <div className="bg-white relative w-full py-8 sm:py-16">
       <div className="flex-1 w-10/12 sm:3/4 mx-auto flex flex-col gap-6 sm:gap-12">
-        <div className="grid sm:grid-cols-2 gap-4 items-center justify-items-center">
-          <Image
-            src={img1.src}
-            alt='donation'
-            width={300}
-            height={300}
-            placeholder='blur'
-            blurDataURL={img1.blurDataURL}
-          />
+        <div className="">
           <p className='text-xl sm:text-3xl font-bold text-center'>
             {t('news1')}
           </p>
+          <div className="mt-4 grid sm:grid-cols-2 gap-4 items-center justify-items-center">
+            <Image
+              src={img2.src}
+              alt='donation'
+              width={300}
+              height={300}
+              placeholder='blur'
+              blurDataURL={img2.blurDataURL}
+            />
+            <Image
+              src={img1.src}
+              alt='donation'
+              width={300}
+              height={300}
+              placeholder='blur'
+              blurDataURL={img1.blurDataURL}
+            />
+          </div>
         </div>
         <div className="grid sm:grid-cols-2 gap-4 items-center justify-items-center">
           <p className='text-xl sm:text-3xl font-bold text-center'>
             {t('news2')}
           </p>
           <Image
-            src={img2.src}
+            src={img3.src}
             alt='donation'
             width={300}
             height={300}
             placeholder='blur'
-            blurDataURL={img2.blurDataURL}
+            blurDataURL={img3.blurDataURL}
           />
         </div>
         <div className="grid sm:grid-cols-2 gap-4 items-center justify-items-center">
           <div>
-            <Image
-              src={img3.src}
-              alt='donation'
-              width={300}
-              height={300}
-              placeholder='blur'
-              blurDataURL={img3.blurDataURL}
-              className='mb-4'
-            />
             <Image
               src={img4.src}
               alt='donation'
